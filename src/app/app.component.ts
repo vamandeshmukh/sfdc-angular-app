e49281f85c37f5bbdb53de04737cd6f8b7a9ea0a
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-10 12:48:45 +0530

    ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+// component - a section that appears on screen
+// component - is made up of four files 
+// .ts, .html, .css, .spec.ts 
+
+// e.g. for header component - 
+// header.component.css  
+// header.component.html - also called as template 
+// header.component.spec.ts - also called as component 
+// header.component.ts   
+// these four files together are called as header component 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,6 +22,7 @@ export class AppComponent {
   title = 'sfdc-angular-app';
   myName = 'Vaman Deshmukh';
   myCity = 'Hyderabad';
+  mySalary = 10.5;
 
 }
 
