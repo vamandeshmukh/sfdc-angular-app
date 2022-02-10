f3ab374c93eb1c8f4a2ccd42eceedcacfa2ba895
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-10 12:13:52 +0530

    C

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sfdc-angular-app';
+  title = 'Vaman Deshmukh';
 }
+
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'sfdc-angular-app';
+// }
