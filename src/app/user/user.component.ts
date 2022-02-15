c099e306102d3bccb606075037b3ecda442c2e62
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-15 11:21:17 +0530

    http

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppUser } from '../models/AppUser';
-import { UserService } from '../user-service/user.service';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
@@ -29,3 +29,37 @@ export class UserComponent implements OnInit {
   }
 
 }
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { AppUser } from '../models/AppUser';
+// import { UserService } from './user.service';
+
+// @Component({
+//   selector: 'app-user',
+//   templateUrl: './user.component.html',
+//   styleUrls: ['./user.component.css']
+// })
+// export class UserComponent implements OnInit {
+
+//   appUser: AppUser = {
+//     username: '',
+//     email: '',
+//     address: {},
+//     phone: ''
+//   };
+
+//   constructor(private userService: UserService) { }
+
+//   getUsers = () => {
+//     console.log('getUsers');
+//     return this.userService.getUserById(1)
+//       .subscribe((u) => { this.appUser = u });
+//   }
+
+//   ngOnInit(): void {
+//     this.getUsers();
+//   }
+
+// }
---
+++ src/app/user/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppUser } from '../models/AppUser';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getUserById = (userId: number): Observable<AppUser> => {
+    console.log('getUsersById');
+    return this.http.get<AppUser>(`https://jsonplaceholder.typicode.com/users/${userId}`);
+  }
+}
+
+
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { AppUser } from '../models/AppUser';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UserService {
+
+//   constructor(private http: HttpClient) { }
+
+//   getUserById = (userId: number): Observable<AppUser> => {
+//     console.log('getUsersById');
+//     return this.http.get<AppUser>(`https://jsonplaceholder.typicode.com/users/${userId}`);
+//   }
+// }
