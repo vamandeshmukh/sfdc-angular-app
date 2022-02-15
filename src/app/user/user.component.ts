a91980e06c8c5a0d155faa2d4555d909f52610dc
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-15 12:38:54 +0530

    http

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,8 +9,8 @@ import { UserService } from './user.service';
 })
 export class UserComponent implements OnInit {
 
-  username: string = '';
   userIdInput: any;
+
   appUser: AppUser = {
     username: '',
     email: '',
